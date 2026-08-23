cbea147cb6b3776de1b4250348de190b0b6be0b3
LUMA Dev <luma-dev@local>
2026-08-23 16:26:55 +0800

    分类栏变白+选中黑边框+帖子垃圾桶图标

--- LIVE/社区/module_trends.js
+++ LIVE/社区/module_trends.js
@@ -187,7 +187,7 @@ function getPostCardHtml(post) {
           </div>\
         </div>\
         <button class="more-btn" onclick="showPostDeleteMenu(\'' + post.id + '\', event)">\
-          <svg class="ic" viewBox="0 0 256 256" fill="currentColor"><path d="M140,128a12,12,0,1,1-12-12A12,12,0,0,1,140,128Zm56-12a12,12,0,1,0,12,12A12,12,0,0,0,196,116ZM60,116a12,12,0,1,0,12,12A12,12,0,0,0,60,116Z"/></svg>\
+          <svg class="ic" viewBox="0 0 256 256" fill="currentColor"><path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192Z"/></svg>\
         </button>\
       </div>\
       <div class="post-body">\
